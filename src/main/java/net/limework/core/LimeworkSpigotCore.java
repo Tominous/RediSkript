157e2b08c77efefd591695176586b255bd7c53e6
Govindass <govindasgm@gmail.com>
2020-10-18 17:07:37 +0300

    Remove unneeded code

--- src/main/java/net/limework/core/LimeworkSpigotCore.java
+++ src/main/java/net/limework/core/LimeworkSpigotCore.java
@@ -1,40 +1,27 @@
 package net.limework.core;
-
-import net.limework.core.guis.ControlGui;
 import net.limework.core.hooks.SkriptHook;
-import net.limework.core.managers.DataSourcesManager;
 import net.limework.core.managers.RedisManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public class LimeworkSpigotCore extends JavaPlugin {
 
     //Redis manager
     private RedisManager rm;
 
-    //data
-    private DataSourcesManager dsm;
-
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         rm = new RedisManager(this);
-        getServer().getPluginCommand("reloadredis").setExecutor(rm);
+        Objects.requireNonNull(getServer().getPluginCommand("reloadredis")).setExecutor(rm);
         if (getServer().getPluginManager().getPlugin("Skript") != null) {
             new SkriptHook(this);
         } else {
             getLogger().info("Skript wasn't found.");
         }
         rm.start();
-        try {
-            ControlGui controlGui = new ControlGui(this);
-            this.getServer().getPluginManager().registerEvents(controlGui, this);
-            this.getCommand("control").setExecutor(controlGui);
-        } catch (NoSuchFieldError e) {
-            getLogger().info("SOMETHING WENT WRONG WHEN LOADING control gui.");
-            e.printStackTrace();
-        }
-        dsm = new DataSourcesManager(this);
 
 
 
@@ -44,15 +31,10 @@ public class LimeworkSpigotCore extends JavaPlugin {
     @Override
     public void onDisable() {
         rm.shutdown();
-        dsm.shutdown();
     }
 
 
     public RedisManager getRm() {
         return rm;
-    }
-
-    public DataSourcesManager getDsm() {
-        return dsm;
     }
 }
